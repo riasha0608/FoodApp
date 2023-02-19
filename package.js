a3a3f3731a7a800f780f733ab90c0ffa66577a2b
riasha24 <riajshah@gmail.com>
2023-02-19 16:19:29 -0500

    Added more food

--- package.js
+++ package.js
@@ -18,5 +18,35 @@ export const shoppingCart = {
         "expiry": 90,
         "link": "https://preppykitchen.com/butter-cookies/",
         "linkName": "Butter Recipe"
+    },
+    "tomatoes" : {
+        "expiry": 14,
+        "link": "https://www.thecookierookie.com/cheesy-baked-tomatoes/",
+        "linkName": "Cheesy Baked Tomatoes"
+    },
+    "cucumbers" : {
+        "expiry": 7,
+        "link": "https://www.foodandwine.com/recipes/cucumber-salad",
+        "linkName": "Cucumber Salad"
+    },
+    "grapes" : {
+        "expiry": 21,
+        "link": "https://www.fabfood4all.co.uk/easy-grape-jam/",
+        "linkName": "Grape Jam"
+    },
+    "blueberries" : {
+        "expiry": 10,
+        "link": "https://amandascookin.com/blueberry-breakfast-cake/",
+        "linkName": "Blueberry Breakfast Cake"
+    },
+    "avocado":{
+        "expiry": 14,
+        "link": "https://www.simplyrecipes.com/recipes/perfect_guacamole/",
+        "linkName": "Guacamole"
+    },
+    "kale": {
+        "expiry": 7,
+        "link": "https://cooking.nytimes.com/recipes/1015707-lemon-garlic-kale-salad",
+        "linkName": "Lemon Garlic Kale Salad"
     }
 }
